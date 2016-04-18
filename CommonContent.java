ba448ae4026239d496721fbb87b726f0ccc16833
JLU-WuYang <albertwyjoy@gmail.com>
2016-04-18 22:23:30 +0800

    Create CommonContent.java

---
+++ CommonContent.java
@@ -0,0 +1,9 @@
+package cn.edu.jlu.ccst.mud;
+
+public interface CommonContent {
+	String CANNOTMOVE = "这个方向没有出路。";
+	enum DIRECTION {
+		EAST, WEST, SOUTH, NORTH, NORTHEAST, NORTHWEST, SOUTHEAST, SOUTHWEST,UP, DOWN 
+	};
+	int CHARS_PER_LINE = 40;
+}
